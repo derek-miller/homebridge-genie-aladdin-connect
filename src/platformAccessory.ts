68a581a79b5ce4ec93a3e94f216a134daf2c7f00
Tim Doering <95541534+HerrTimD@users.noreply.github.com>
2022-12-18 15:52:30 -0500

    Prevent re-setting TargetDoorState to same value
    Bolt from method to change the TargetDoorState characteristic, if and only if current TargetDoorState and requested TargetDoorState are identical.

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -196,6 +196,17 @@ export class GenieAladdinConnectGarageDoorAccessory {
 
   private async setTargetDoorState(value: CharacteristicValue): Promise<void> {
     const desiredStatus = this.convertTargetStateValueToDesiredStatus(value);
+    if (
+      desiredStatus === this.desiredStatus
+    ) {
+      this.log.debug(
+        '[%s] Set Characteristic TargetDoorState -> %s, already set TargetDoorState -> %s. Cancelling.',
+        this.door.name,
+        AladdinDesiredDoorStatus[desiredStatus],
+        AladdinDoorStatus[this.desiredStatus],
+      );
+      return;
+    }
     this.log.debug(
       '[%s] Set Characteristic TargetDoorState ->',
       this.door.name,
